2c3ec0b76af648f0ae5c00568ebd2cf89c3aeb57
Patrik Nygren <patriknygren82@gmail.com>
2016-11-27 20:42:45 +0100

    experimenting with kryonet

--- src/main/java/com/multipong/server/Main.java
+++ src/main/java/com/multipong/server/Main.java
@@ -1,9 +1,15 @@
 package com.multipong.server;
 
+import java.io.IOException;
+
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("hello world");
+		try {
+			new GameServer();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
